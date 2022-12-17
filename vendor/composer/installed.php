f9df26571f27c37018561c343930c7f80a6f558f
Juan <6652831+quadlayers@users.noreply.github.com>
2022-12-16 21:43:20 -0300

    First commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '20cea4afc92a7d4cf6ac6b464903739742f8e1ed',
+        'name' => 'quadlayers/wp-dashboard-widget-news',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'quadlayers/wp-dashboard-widget-news' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '20cea4afc92a7d4cf6ac6b464903739742f8e1ed',
+            'dev_requirement' => false,
+        ),
+    ),
+);
